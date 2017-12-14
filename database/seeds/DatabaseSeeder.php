56534d9a6fbb524a9d2aaf32a8819ec9b51df485
Renant Bernabe <contato@renant.com.br>
2017-12-14 13:09:10 -0200

    Gerador Fake de Usuários

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Entities\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(UsersTableSeeder::class);
+        $this->developer();
+        //$this->production();
+
+    }
+
+    // Gerador de informações fake para popular o banco
+    private function developer()
+    {
+        $this->call(UsersTableSeeder::class);
+        factory(User::class, 150)->create();
+    }
+
+    // Gera apenas as informações basicas para rodar o sistema
+    private function production()
+    {
+        $this->call(UsersTableSeeder::class);
     }
 }
