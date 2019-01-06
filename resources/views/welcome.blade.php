ffaf06ea92a12cb7e8b80ff43ecd9ca37beb4a8d
Renant Bernabe <contato@renant.com.br>
2019-01-06 02:06:34 -0500

    Nova Build

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,21 +2,19 @@
 <html lang="{{ app()->getLocale() }}">
     <head>
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
-        <meta charset="utf-8">
-        <title>TangoSeed</title>
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
+  <meta charset="utf-8">
+  <title>TangoSeed</title>
 
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" type="image/x-icon" href="favicon.ico">
-        <link rel="stylesheet" href="styles.1b6b84d697e9d4e69d20.css"></head>
-    </head>
-    <body>
-        <app-root></app-root>
-
-        <script type="text/javascript" src="runtime.ec2944dd8b20ec099bf3.js"></script>
-        <script type="text/javascript" src="polyfills.3f11bf9959f924754af3.js"></script>
-        <script type="text/javascript" src="scripts.6fb24ad5370e2ea9eef9.js"></script>
-        <script type="text/javascript" src="main.242727253ca2777293b0.js"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="build/favicon.ico">
+<link rel="stylesheet" href="build/styles.1b6b84d697e9d4e69d20.css"></head>
+<body>
+  <app-root></app-root>
+<script type="text/javascript" src="build/runtime.ec2944dd8b20ec099bf3.js"></script>
+<script type="text/javascript" src="build/polyfills.3f11bf9959f924754af3.js"></script>
+<script type="text/javascript" src="build/scripts.6fb24ad5370e2ea9eef9.js"></script>
+<script type="text/javascript" src="build/main.815cb47ca6b7a1c960fe.js"></script>
 
     </body>
 </html>
