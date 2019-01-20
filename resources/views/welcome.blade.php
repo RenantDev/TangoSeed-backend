0e45d2af3278ca9365dcf0810ba4e693814aa7a0
Renant Bernabe <contato@renant.com.br>
2019-01-20 01:46:23 -0500

    update frontend

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,13 +8,13 @@
 
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
-<link rel="stylesheet" href="styles.1b6b84d697e9d4e69d20.css"></head>
-<body>
-  <app-root></app-root>
-<script type="text/javascript" src="runtime.ec2944dd8b20ec099bf3.js"></script>
-<script type="text/javascript" src="polyfills.3f11bf9959f924754af3.js"></script>
-<script type="text/javascript" src="scripts.6fb24ad5370e2ea9eef9.js"></script>
-<script type="text/javascript" src="main.815cb47ca6b7a1c960fe.js"></script>
-
-    </body>
+  <link rel="stylesheet" href="styles.3833ec4a149d1d242f9a.css"></head>
+  <body>
+    <app-root></app-root>
+  <script type="text/javascript" src="runtime.ec2944dd8b20ec099bf3.js"></script>
+  <script type="text/javascript" src="polyfills.d2a114715ee4d0a59fcc.js"></script>
+  <script type="text/javascript" src="scripts.6fb24ad5370e2ea9eef9.js"></script>
+  <script type="text/javascript" src="main.1ab06d0180507cb8ca37.js"></script>
+</body>
+  
 </html>
